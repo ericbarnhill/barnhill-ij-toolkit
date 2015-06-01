467e5e7a55afe5b2a0279a7371de6a39c26d6b95
Eric <ericbarnhill@protonmail.ch>
2015-06-01 11:07:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# barnhill_ij_toolkit alpha (c) 2015 Eric Barnhill.
---
+++ src/ijtoolkit/Selection_To_Indices.java
@@ -0,0 +1,58 @@
+package ijtoolkit;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.Roi;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.FloatPolygon;
+import ij.process.ImageProcessor;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+public class Selection_To_Indices implements PlugInFilter {
+	
+	int h, w, d;
+	ImagePlus imp;
+	
+	@Override
+	public void run(ImageProcessor ip) {
+		Roi roi = imp.getRoi();
+		FloatPolygon fp = roi.getFloatPolygon();
+		StringBuilder sb = new StringBuilder();
+		int index = 0;
+		/*
+		for (int i = 0; i < fp.npoints; i++) {
+			index = fp.xpoints[i] + w*fp.ypoints[i];
+			sb.append(Integer.toString((int)index));
+			sb.append(",");
+		}
+		*/
+		for (int x = 0; x < w; x++) {
+			for (int y = 0; y < h; y++) {
+				if ( roi.contains(x, y) ) {
+					index = x + y*w;
+					sb.append(Integer.toString(index));
+					sb.append(",");
+				}
+			}
+			sb.append("...");
+			sb.append(System.getProperty("line.separator"));
+		}
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Clipboard clipboard = toolkit.getSystemClipboard();
+        clipboard.setContents(new StringSelection(sb.toString()), null);
+		
+	}
+
+	@Override
+	public int setup(String arg, ImagePlus imp) {
+		this.imp = imp;
+		h = imp.getHeight();
+		w = imp.getWidth();
+		d = imp.getImageStackSize();
+		return DOES_ALL;
+	}
+
+}
