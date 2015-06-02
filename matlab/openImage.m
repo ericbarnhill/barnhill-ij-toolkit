091c2564b5f07c19fee58b796c17870e96a2b102
Eric <ericbarnhill@protonmail.ch>
2015-06-02 12:11:40 +0100

    push of selection<->indices utilities

--- matlab/openImage.m
+++ matlab/openImage.m
@@ -1,9 +1,13 @@
-function [resh] =openImage(image, mij_instance)
+function [resh] = openImage(name, image, mij_instance)
+%% openImage (c) Eric Barnhill 2015. All rights reserved. MIT License, see README.
+% This script will automatically open a Matlab object of any dimension >= 2
+% in the present instance of MIJ
 a = pwd;
 sz = size(image);
 thirdDim = 1;
-for n = 1: (numel(sz)-2);
+for n = 1: (numel(sz)-2);edit roi_to_vector
+
     thirdDim = thirdDim * sz(n+2);
 end
 resh = reshape(image, sz(1), sz(2), thirdDim);
-mij_instance.createImage(resh);
+mij_instance.createImage(name, resh, 1);
